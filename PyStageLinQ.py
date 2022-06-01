a1d81c6da40acdf104aa258d024a8d848f664916
Jaxc <jaxc@skrooter.com>
2022-06-01 23:33:32 +0200

    Add inital PyStageLinQ code
    Code can so far read Discovery messages sent by Prime Go.

---
+++ PyStageLinQ.py
@@ -0,0 +1,119 @@
+
+"""
+License TBD
+"""
+import select
+import socket
+import time
+
+class PyStageLinQ:
+
+    # PyStageLinQ Error codes
+    STAGELINQOK         = 0
+
+    DISCOVERYTIMEOUT    = 100
+
+    INVALIDFRAME        = 200
+    MAGICFLAGNOTFOUND   = 201
+
+
+    def __init__(self):
+        pass
+
+    def discoverStageLinQDevice(self, timeout = 10):
+        """
+        This function can be used to find StageLinQ devices.
+        """
+
+        # Local Constants
+        StageLinQPort = 51337
+        DiscoverBufferSize = 8192
+
+        # Create socket
+        discoverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        discoverSocket.bind(("", StageLinQPort)) # bind socket to all interfaces
+        discoverSocket.setblocking(0)
+
+        loop_timeout = time.time() + timeout
+
+        while True:
+            dataAvailable = select.select([discoverSocket], [], [], loop_timeout - time.time() )
+            if dataAvailable[0]:
+                data, addr = discoverSocket.recvfrom(DiscoverBufferSize)
+                print("received message: %s" % data)
+                if self.STAGELINQOK != self.decodeDiscoveryFrame(data) :
+                    continue
+
+                #self.connectToStageLinQDevice(addr[0], addr[1])
+
+
+            if time.time() > loop_timeout:
+                return self.DISCOVERYTIMEOUT
+
+
+    def decodeDiscoveryFrame(self, Frame):
+
+        # Local Constants
+        StageLinQMagicFlag = bytes('airD'.encode(encoding="ASCII"))
+        MagicFlagStart          = 0
+        MagicFlagLength         = 4
+        MagicFlagStop           = MagicFlagStart + MagicFlagLength
+        TokenStart              = MagicFlagStop
+        TokenLength             = 16
+        TokenStop               = TokenStart + TokenLength
+        DeviceNameSizeStart     = TokenStop
+        PortLength              = 2
+
+
+
+        if len(Frame) < 4:
+            return self.INVALIDFRAME
+
+        # Check if Frame contains Magic flag
+        if StageLinQMagicFlag != Frame[MagicFlagStart:MagicFlagStop]:
+            return self.MAGICFLAGNOTFOUND
+
+        self.token = Frame[TokenStart:TokenStop]
+
+
+
+
+        ConnectionTypeStart, DeviceName = self.ReadBytesFromFrame(Frame, DeviceNameSizeStart)
+        SwNameStart, ConnectionType = self.ReadBytesFromFrame(Frame, ConnectionTypeStart)
+        SwVersionStart, SwName = self.ReadBytesFromFrame(Frame, SwNameStart)
+        PortStart, SwVersion = self.ReadBytesFromFrame(Frame, SwVersionStart)
+        PortStop = PortStart + PortLength
+        Port = int.from_bytes(Frame[PortStart:PortStop], byteorder='big')
+
+        print(f"Device: {DeviceName}, ConnectionType: {ConnectionType}, SwName: {SwName}, SwVersion: {SwVersion}, Port: {Port}")
+        return self.STAGELINQOK
+
+    def ReadBytesFromFrame(self, Frame, StartOffset):
+
+        # Only uint32 length supported
+        SizeLength    = 4
+        SizeStop      = StartOffset + SizeLength
+        DataStart     = SizeStop
+
+        DataLength = int.from_bytes(Frame[StartOffset:SizeStop], byteorder='big')
+        DataStop = DataStart + DataLength
+
+        if DataStop > len(Frame):
+            # Out of bounds
+            return
+
+        return DataStop, Frame[DataStart:DataStop].decode(encoding='UTF-8')
+
+    def connectToStageLinQDevice(self, ip, port):
+
+
+        self.StageLinQSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.StageLinQSocket.connect(ip, port)
+
+        pass
+
+
+if __name__ == "__main__":
+    # Test code for class
+    StageLinQ = PyStageLinQ()
+    StageLinQ.discoverStageLinQDevice(timeout=1)
