0fc63a8099747b04ee809662f63928443423037d
Jaxc <jaxc@skrooter.com>
2025-12-29 19:00:30 +0100

    Update build requirements version

--- make_release.sh
+++ make_release.sh
@@ -11,9 +11,11 @@ sed -i 's/version = "0.0.0"/version = "'$new_ver'"/' pyproject.toml
 sed -i 's/release = "0.0.0"/release = "'$new_ver'"/' docs/conf.py
 sed -i 's/SwVersion="0.0.1"/SwVersion="'$new_ver'"/' PyStageLinQ/PyStageLinQ.py
 
-
+echo "Removing old files"
 rm dist/* -f
 
+echo "Build release"
 python3 -m build
 
+echo "upload release to pypi"
 twine upload dist/* -u __token__ -p $1
